3421c495fd959f9142ae3f358b179b5ac0bf5a47
Kamil MARKOWSKI <kamilmarkowski@MacBook-Air-de-Kamil.local>
2024-01-15 18:40:48 +0100

    Coding environment has been set up

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+const container = document.getElementById("app");
+const root = createRoot(container);
+
+function App() {
+    return <h1>The coding environment is ready for magic</h1>
+}
+
+root.render(<App />);
